cb8e36b16773a97444e64862a98bc0a57e3238eb
zekiblue <zekiberkulu@gmail.com>
2021-09-26 23:12:28 +0200

    Make the saving structure better

--- drivy_business/main.py
+++ drivy_business/main.py
@@ -1,31 +1,28 @@
 import asyncio
+import csv
 from typing import List
 
 import typer
 
 from drivy_business.src.drivy_api import DrivyAPI
-from drivy_business.src.enums import year_id_map, km_id_map, CITY_GENT
-from drivy_business.src.models import VehicleModel
-from drivy_business.src.utils import get_all_earnings
+from drivy_business.src.enums import CITY_GENT
+from drivy_business.src.utils import get_all_earnings, save_csv
 
 app = typer.Typer()
 
 
 @app.command()
 def get_earnings():
+    verbose = False
     loop = asyncio.get_event_loop()
-    drivy_api = DrivyAPI()
+    drivy_api = DrivyAPI(verbose=verbose)
     city = CITY_GENT.copy()
 
-    loop.run_until_complete(get_all_earnings(drivy_api, city))
-    # brand_id = 2
-    # # models: List[VehicleModel] = asyncio.run(drivy_api.get_models(brand_id))
-    # # model_id = models[0].id
-    # model_id = 11
-    # year_id = list(year_id_map.keys())[0]
-    # km_id = list(km_id_map.keys())[0]
-    # response = loop.run_until_complete(drivy_api.get_estimated_earning(brand_id, model_id, year_id, km_id, city))
-    # loop.run_until_complete(drivy_api.close())
+    results = loop.run_until_complete(get_all_earnings(drivy_api, city, verbose))
+
+    save_csv(header=list(results[0].keys()), results=results, name_of_csv="all_brands")
+
+    loop.run_until_complete(drivy_api.close())
 
 
 if __name__ == '__main__':
--- drivy_business/src/utils.py
+++ drivy_business/src/utils.py
@@ -1,18 +1,22 @@
+import csv
+from typing import List
+
 from drivy_business.src.drivy_api import DrivyAPI
 from drivy_business.src.enums import brand_id_map, year_id_map, km_id_map
 from drivy_business.src.models import CityDetails
 
 
-async def get_all_earnings(drivy_api: DrivyAPI, city: CityDetails):
-    results = []
+async def get_all_earnings(drivy_api: DrivyAPI, city: CityDetails, verbose: bool = True):
+    general_results = []
     for brand_id in list(brand_id_map.keys()):
+        brand_results = []
         models = await drivy_api.get_models(brand_id)
         for model in models:
             model_id = model.id
             for year_id in list(year_id_map.keys()):
                 for km_id in list(km_id_map.keys()):
                     earning = await drivy_api.get_estimated_earning(brand_id, model_id, year_id, km_id, city)
-                    results.append(
+                    brand_results.append(
                         {
                             "brand": brand_id_map.get(brand_id),
                             "model": model.localized_label,
@@ -21,5 +25,20 @@ async def get_all_earnings(drivy_api: DrivyAPI, city: CityDetails):
                             "earning": earning
                         }
                     )
-        print(results)
-        break
+        if verbose:
+            print(brand_results)
+        save_csv(header=list(brand_results[0].keys()), results=brand_results, name_of_csv=brand_id_map.get(brand_id))
+        general_results.extend(brand_results)
+    return general_results
+
+
+def save_csv(*, header: List[str] = None, results: List[dict], name_of_csv: str):
+    if not header:
+        header = results[0].keys()
+
+    directory = f"results/{name_of_csv}.csv"
+    with open(directory, "w") as handle:
+        dict_writer = csv.DictWriter(handle, header)
+        dict_writer.writeheader()
+        dict_writer.writerows(results)
+    print(f"Csv saved to the {directory}")
