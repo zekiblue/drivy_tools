a6858a786cc8f7c9c55edfe3970d7d0e46d4fcde
zekiblue <zekiberkulu@gmail.com>
2021-09-27 22:34:49 +0200

    Change default configs

--- drivy_tools/src/config.py
+++ drivy_tools/src/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseConfig
 
 
 class Config(BaseConfig):
-    httpx_max_keepalive_conn = 5
-    httpx_max_conn = 10
+    httpx_max_keepalive_conn = 2
+    httpx_max_conn = 5
 
 
 config = Config()
