9f486cc120c0a619a4eb1d61a3586c8b2136ecc5
zekiblue <zekiberkulu@gmail.com>
2021-10-05 20:33:01 +0200

    Change client structure

--- drivy_tools/src/client.py
+++ drivy_tools/src/client.py
@@ -13,33 +13,36 @@ class HTTPClient(BaseModel):
     async_enabled: bool = True
     proxies: List[str] = None
 
+    max_keepalive_connections: int = None
+    max_connections: int = None
+
     async def _request(self, *args, **kwargs):
         method = kwargs.pop("method")
         kwargs["headers"] = kwargs.get("headers", {"User-Agent": generate_user_agent()})
-        if state.config.DEFAULT.proxy_enabled:
-            # kwargs["proxies"] = self.get_random_proxy()
-            self.client.proxies = self.get_random_proxy()
+
         if self.async_enabled:
-            resp = await getattr(self.client, method)(*args, **kwargs)
+            max_keepalive_conn = (
+                self.max_keepalive_connections
+                if self.max_keepalive_connections
+                else state.config.HTTPX.httpx_max_keepalive_conn
+            )
+            max_conn = self.max_connections if self.max_connections else state.config.HTTPX.httpx_max_conn
+            limits = httpx.Limits(max_keepalive_connections=max_keepalive_conn, max_connections=max_conn)
+            proxies = self.get_random_proxy() if state.config.DEFAULT.async_enabled else None
+            async with httpx.AsyncClient(limits=limits, timeout=state.config.HTTPX.timeout, proxies=proxies) as client:
+                resp = await getattr(client, method)(*args, **kwargs)
         else:
-            resp = getattr(self.client, method)(*args, **kwargs)
+            proxies = self.get_random_proxy() if state.config.DEFAULT.async_enabled else None
+            with httpx.Client(timeout=state.config.HTTPX.timeout, proxies=proxies) as client:
+                resp = getattr(client, method)(*args, **kwargs)
 
         return resp
 
-    def __init__(self, **data: Any):
-        super().__init__(**data)
-        timeout = data.pop("timeout", state.config.HTTPX.timeout)
-        if self.async_enabled:
-            max_keepalive_conn = data.pop("max_keepalive_connections", state.config.HTTPX.httpx_max_keepalive_conn)
-            max_conn = data.pop("max_connections", state.config.HTTPX.httpx_max_conn)
-            limits = httpx.Limits(max_keepalive_connections=max_keepalive_conn, max_connections=max_conn)
-            self.client = httpx.AsyncClient(limits=limits, timeout=timeout)
-        else:
-            self.client = httpx.Client(timeout=timeout)
-
     def get_random_proxy(self):
         proxy = random.choice(self.proxies)
-        return {"http": proxy, "https": proxy}
+        if proxy.startswith("http://"):
+            proxy = "http://" + proxy
+        return {"http://": proxy, "https://": proxy}
 
     async def get(self, *args, **kwargs):
         return await self._request(method="get", *args, **kwargs)
